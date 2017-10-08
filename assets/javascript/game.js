f9b94b50ad825bab1dec6e70b0ee7a7b41e1e997
Sean Hagan <sphagan1088@yahoo.com>
2017-10-07 23:16:29 -0700

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,52 +14,95 @@
 //wins).
 
 //list of variables
-var letterGuess 
+
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;  
-var guessesSoFar 
+var guessesSoFar = [];
+
+
+
+
 
 //array of letters 
 var letter = 
 	["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//user presses a key to make a guess
-document.onkeyup = function(event){
-var letterGuess = event.key.toLowerCase();
-alert(letterGuess);
-
-}
 
 // computer selects random letter 
 var computerLetter = letter[Math.floor(Math.random()*letter.length)];
 console.log(computerLetter);
 
+//user presses a key to make a guess
+document.onkeyup = function(event){
+	var letterGuess = event.key.toLowerCase();
+		guessesSoFar.push(letterGuess);
+
+
+
+
 //if statments for letterGuess
 
 
 	if ((letterGuess) === (computerLetter)){
-		wins++
+		wins++;
+		reset();
+		 computerLetter = letter[Math.floor(Math.random()*letter.length)];
+         console.log(computerLetter)
+	
+
+		
+	
+
+	} else {
+		guessesLeft--;
+	
+	}
+		
+
+	
+
+	 if (guessesLeft === 0) {
+		losses++;
+		reset();
+		 computerLetter = letter[Math.floor(Math.random()*letter.length)];
+         console.log(computerLetter)
+	
 	}
 
-	if ((letterGuess) != (computerLetter)) {
-		guessesLeft--
+	//resets guesses after win or ls
+	function reset() {
+		guessesLeft = 10;
+		guessesSoFar = [];
+		
+		
+		
+	
 	}
 
-	if ((guessesLeft)===  0 ) {
-		losses++
-	}
 
+	
 
+ 	// add html to page 
 
-
-
-	// add html to page 
-
-	var html = "<p> Guess What Letter I'm Thinking Of<p>" + 
+	var html = "<p> Guess What Letter I'm Thinking Of</p>" + 
 	"<p> Wins:" + wins + "</p>" + 
 	"<p> Losses:" + losses + "</p>" +
-	"<p> Guesses Left:" + guessesLeft + "</p>"
-	"<p> Guesses so far:" + guessesSoFar + "</p>";
+	"<p> Guesses Left:" + guessesLeft + "</p>" +
+	"<p> Guesses So Far:" + guessesSoFar + "</p>";
 
-	document.querySelector("#game").innerHTML = html;
+
+
+	document.querySelector("#game").innerHTML = html; 
+
+	
+};
+
+
+
+
+
+
+
+
+
